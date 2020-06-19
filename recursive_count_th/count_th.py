d496920dd6a3ac3e05b9326fadc9deca94e7973c
Michael Walter <michael@MacBook-Pro.hitronhub.home>
2020-06-19 10:15:09 -0600

    Completed count_th.py, all tests passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,24 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    # Convert all to lowercase
+    ## This isn't needed
+    # word = word.lowercase()
+    """
+    Needs to count the number of appearances of the lowercase substring 'th' in the string var 'word'.
+    """
+    # Find the index location of the first 'th'
+    idx = word.find('th')
+
+    # If no substring found, exit stage right - return 0
+    if idx == -1:
+        return 0
+
+    # recursively call, and iterate a count variable in place
+    # within the return
+
+    return 1 + count_th(word[idx + 2:])
+
+
+
+    # pass
